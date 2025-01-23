24edf7250795e726cbce1fc556364c6ba183ca76
Alexsandr <manllac35@gmail.com>
2025-01-23 21:47:45 +0400

    homeWork1

---
+++ Homework/Homework/Dictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Dictionary
+    {
+        public void TaskLoop()
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+
+            while (true)
+            {
+                Console.WriteLine("Введите имя студента:");
+                string studentName = Console.ReadLine();
+
+                double grade;
+                while (true)
+                {
+                    Console.WriteLine("Введите среднюю оценку студента от 2 до 5:");
+                    if(double.TryParse(Console.ReadLine(), out grade) && grade >= 2 && grade <=5 )
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод. Оценка должна быть от 2 до 5.");
+                    }
+                }
+
+                students[studentName] = grade;
+                Console.WriteLine("\nВсе студенты и их оценки:");
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"{student.Key}: {student.Value}");
+                }
+
+                Console.WriteLine("\nВведите имя студента для поиска оценки:");
+                string searchName = Console.ReadLine();
+                if (students.ContainsKey(searchName))
+                {
+                    Console.WriteLine($"{searchName} имеет оценку {students[searchName]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Студент с именем {searchName} не найден.");
+                }
+
+                Console.WriteLine("\nВведите \"–exit\" для выхода.");
+                if (Console.ReadLine() == "–exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Homework/Homework/List.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class List
+    {
+        public void TaskLoop()
+        {
+            List<string> strings = new List<string> { "Осень", "Зима", "Весна" };
+
+            Console.WriteLine("Первоначальный список:");
+            foreach (var item in strings) 
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Введите месяц для добавляения в список:");
+            string strings2 = Console.ReadLine();
+            strings.Add(strings2);
+
+            Console.WriteLine("Содержимое списка после добавления:");
+            foreach(var item in strings2)
+            { 
+                Console.WriteLine(item); 
+            }
+
+            Console.WriteLine("Введите месяц для добавляения в середину списка:");
+            string strings3 = Console.ReadLine();
+            int midString = strings.Count / 2;
+            strings.Insert(midString, strings3);
+
+            Console.WriteLine("Содержимое списка после добавления в середину:");
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nВведите \" - exit\" для выхода");
+            if (Console.ReadLine()== "-exite")
+            {
+                return;
+            }
+        }
+
+    }
+}
