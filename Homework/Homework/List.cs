3f283de4e9eb120ce972608b75540fe38a257d10
Alexsandr <manllac35@gmail.com>
2025-01-26 14:06:12 +0400

    home2

--- Homework/Homework/List.cs
+++ Homework/Homework/List.cs
@@ -10,39 +10,42 @@ namespace Homework
     {
         public void TaskLoop()
         {
-            List<string> strings = new List<string> { "Осень", "Зима", "Весна" };
-
-            Console.WriteLine("Первоначальный список:");
-            foreach (var item in strings) 
+            while (true)
             {
-                Console.WriteLine(item);
-            }
+                List<string> strings = new List<string> { "Осень", "Зима", "Весна" };
 
-            Console.WriteLine("Введите месяц для добавляения в список:");
-            string strings2 = Console.ReadLine();
-            strings.Add(strings2);
+                Console.WriteLine("Первоначальный список:");
+                foreach (var item in strings)
+                {
+                    Console.WriteLine(item);
+                }
 
-            Console.WriteLine("Содержимое списка после добавления:");
-            foreach(var item in strings2)
-            { 
-                Console.WriteLine(item); 
-            }
+                Console.WriteLine("Введите месяц для добавляения в список:");
+                string strings2 = Console.ReadLine();
+                strings.Add(strings2);
 
-            Console.WriteLine("Введите месяц для добавляения в середину списка:");
-            string strings3 = Console.ReadLine();
-            int midString = strings.Count / 2;
-            strings.Insert(midString, strings3);
+                Console.WriteLine("Содержимое списка после добавления:");
+                foreach (var item in strings)
+                {
+                    Console.WriteLine(item);
+                }
 
-            Console.WriteLine("Содержимое списка после добавления в середину:");
-            foreach (var item in strings)
-            {
-                Console.WriteLine(item);
-            }
+                Console.WriteLine("Введите месяц для добавляения в середину списка:");
+                string strings3 = Console.ReadLine();
+                int midString = strings.Count / 2;
+                strings.Insert(midString, strings3);
 
-            Console.WriteLine("\nВведите \" - exit\" для выхода");
-            if (Console.ReadLine()== "-exite")
-            {
-                return;
+                Console.WriteLine("Содержимое списка после добавления в середину:");
+                foreach (var item in strings)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("\nВведите \" exit\" для выхода");
+                if (Console.ReadLine() == "exit")
+                {
+                    return;
+                }
             }
         }
 
--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Выберите задачу (1 - Список, 2 - Словарь, 0 - Выход):");
+                int task;
+                if (!int.TryParse(Console.ReadLine(), out task))
+                {
+                    Console.WriteLine("Некорректный ввод, попробуйте снова.");
+                    continue;
+                }
+
+                switch (task)
+                {
+                    case 1:
+                        var listTask = new List();
+                        listTask.TaskLoop();
+                        break;
+                    case 2:
+                        var dictionTask = new Diction();
+                        dictionTask.TaskLoop();
+                        break;
+                    case 0:
+                        Console.WriteLine("Выход из программы.");
+                        return; 
+                    default:
+                        Console.WriteLine("Неверный выбор, попробуйте снова.");
+                        break;
+                }
+            }
         }
     }
 }
